8dc1f3b3f64038246456c8d8e75d28ba408106c8
Joseph Migbodzi <josephmacoy52@gmail.com>
2025-10-31 02:35:18 +0000

    Update config/db.js

--- config/db.js
+++ config/db.js
@@ -7,10 +7,10 @@ dotenv.config(); // Load environment variables
 
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  //host: process.env.DB_HOST,
+  //user: process.env.DB_USER,
+  //password: process.env.DB_PASSWORD,
+  database: process.env.DB_URL,
   port: process.env.DB_PORT || 3306,
 });
 
