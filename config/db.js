f2db6d85d795b19d283b6f09691d7316a3457a87
Joseph Migbodzi <josephmacoy52@gmail.com>
2025-10-31 02:49:55 +0000

    Update database configuration and server setup

--- config/db.js
+++ config/db.js
@@ -7,9 +7,7 @@ dotenv.config(); // Load environment variables
 
 
 const db = mysql.createConnection({
-  //host: process.env.DB_HOST,
-  //user: process.env.DB_USER,
-  //password: process.env.DB_PASSWORD,
+  
   database: process.env.DB_URL,
   port: process.env.DB_PORT || 3306,
 });
